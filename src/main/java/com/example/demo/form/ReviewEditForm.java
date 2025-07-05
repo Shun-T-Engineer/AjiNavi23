04d78d55e765427741fe228960fedf7b44835876
Shun-T-Engineer <shun.0520.1988@gmail.com>
2025-07-06 02:45:57 +0900

    ReviewEditFormの修正

--- src/main/java/com/example/demo/form/ReviewEditForm.java
+++ src/main/java/com/example/demo/form/ReviewEditForm.java
@@ -14,10 +14,12 @@ import lombok.Data;
 public class ReviewEditForm {
 	@NotNull(message="入力してください。")
 	@Min(value=1,  message="正の整数を入力してください。")
-	private Integer reviewId;
+	private Integer restaurantId;
 	
 	@NotNull(message="入力してください。")
 	@Min(value=1,  message="正の整数を入力してください。")
+	private Integer reviewId;
+	
 	private String restaurantName;
 
 	@Size(min=4, max=16, message="4文字から16文字で指定してください。")
